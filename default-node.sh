979434ec10a2eb1a0bb10947c3801a2e7a9b421f
Manuel <Mplonski@t-online.de>
2019-03-06 16:45:40 +0100

    Update default-node.sh

--- default-node.sh
+++ default-node.sh
@@ -7,8 +7,7 @@ apt-get -y install \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common
-	
+    software-properties-common	
 #Add Repos
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -17,27 +16,18 @@ add-apt-repository \
    $(lsb_release -cs) \
    stable"
 add-apt-repository ppa:wireguard/wireguard
-
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-
 # Update and Upgrade
 apt-get -y update 
 apt-get -y upgrade
-
 # Install wireguard/wireguard
-
 apt-get -y install wireguard
-
 # Install Docker
 apt-get install docker-ce docker-ce-cli containerd.io
 # Install Kube-Systems
-
 apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
-
-
-
 #Done
 echo "Done"
