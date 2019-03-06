8ff25b052e253a402021ff6bd74bb630a53d3973
Manuel <Mplonski@t-online.de>
2019-03-06 16:57:49 +0100

    Update default-node.sh

--- default-node.sh
+++ default-node.sh
@@ -5,12 +5,10 @@ sudo su
 apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 #Add Repos
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 add-apt-repository ppa:wireguard/wireguard
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 # Update and Upgrade
 apt-get -y update 
 apt-get -y upgrade
